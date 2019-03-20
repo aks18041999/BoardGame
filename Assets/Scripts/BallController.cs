bf8affc520b3f0b347a90501a7845d5c7cf19e73
Ayush Kumar Shukla <aks18041999@gmail.com>
2019-03-20 10:14:58 +0530

    FisrtCommit
    Single ballmovement (NO Restriction)

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        position = rigidbody.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide");
+        if(other.gameObject.tag == "Goal")
+        {
+            Debug.Log("CollideFGoal");
+            rigidbody.position = position;
+            rigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+}
